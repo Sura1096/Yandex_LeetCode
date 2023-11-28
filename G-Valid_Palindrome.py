4ca31ab16b86e9e3d5b8b05530a56307449c900f
Sura <suri.murodova@gmail.com>
2023-11-28 10:13:20 +0300

    Add another solution with two pointers

--- G-Valid_Palindrome.py
+++ G-Valid_Palindrome.py
@@ -30,9 +30,20 @@ s consists only of printable ASCII characters.
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        a = ''.join([char for char in s if char.isalnum()]).lower()
+        start = 0
+        end = len(s) - 1
 
-        return a == a[::-1]
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
+            elif not s[end].isalnum():
+                end -= 1
+            elif s[start].lower() == s[end].lower():
+                start += 1
+                end -= 1
+            else:
+                return False
+        return True
 
 
 sol = Solution()
