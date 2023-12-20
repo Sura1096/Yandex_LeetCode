d316edf59f170433fbae9e26a22fa153c7ed01fc
Sura <suri.murodova@gmail.com>
2023-12-20 08:25:48 +0300

    Add testcases

--- M-Two_Sum.py
+++ M-Two_Sum.py
@@ -35,3 +35,9 @@ Follow-up: Can you come up with an algorithm that is less than O(n2) time comple
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         pass
+
+
+sol = Solution()
+print(sol.twoSum(nums=[2, 7, 11, 15], target=9))
+print(sol.twoSum(nums=[3, 2, 4], target=6))
+print(sol.twoSum(nums=[3, 3], target=6))
