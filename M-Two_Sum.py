831410789565a6316c286995404d9400635b4292
Sura <suri.murodova@gmail.com>
2023-12-20 08:23:59 +0300

    Add twoSum method with no logic

--- M-Two_Sum.py
+++ M-Two_Sum.py
@@ -30,3 +30,8 @@ Only one valid answer exists.
 
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 '''
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
