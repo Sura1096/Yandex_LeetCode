1df672f7245f75a4f1a2f6454c806dad30af93e4
Sura <suri.murodova@gmail.com>
2023-12-28 09:06:49 +0300

    Add TreeNode class with init constructor

--- Q-Validate_Binary_Search_Tree.py
+++ Q-Validate_Binary_Search_Tree.py
@@ -21,3 +21,11 @@ Constraints:
 The number of nodes in the tree is in the range [1, 104].
 -231 <= Node.val <= 231 - 1
 '''
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
