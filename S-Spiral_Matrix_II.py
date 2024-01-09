b93e7c4c846acfdcbe2ac4a4d0792b8d1974e090
Sura <suri.murodova@gmail.com>
2024-01-09 11:11:08 +0300

    Add logic for generateMatrix method

--- S-Spiral_Matrix_II.py
+++ S-Spiral_Matrix_II.py
@@ -21,4 +21,34 @@ Constraints:
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        pass
+        matrix = [[0] * n for _ in range(n)]
+        L = T = 0
+        R = B = n - 1
+        ans = 1
+
+        while L <= R:
+            # Fill top row
+            for col in range(L, R + 1):
+                matrix[T][col] = ans
+                ans += 1
+            T += 1
+
+            # Fill right column
+            for r in range(T, B + 1):
+                matrix[r][R] = ans
+                ans += 1
+            R -= 1
+
+            # Fill bottom (reverse order)
+            for col in range(R, L - 1, -1):
+                matrix[B][col] = ans
+                ans += 1
+            B -= 1
+
+            # Fill left column (reverse order)
+            for l in range(B, T - 1, -1):
+                matrix[l][L] = ans
+                ans += 1
+            L += 1
+
+        return matrix
