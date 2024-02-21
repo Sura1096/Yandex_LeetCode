c2acb5af57269f73c025b6cdced64eb89b8c9220
Sura <suri.murodova@gmail.com>
2024-02-21 11:15:40 +0300

    Add Solution class and its intersect method with no logic

--- Z-Intersection_of_Two_Arrays_II.py
+++ Z-Intersection_of_Two_Arrays_II.py
@@ -27,3 +27,8 @@ What if nums1's size is small compared to nums2's size? Which algorithm is bette
 What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into 
 the memory at once?
 '''
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        pass
