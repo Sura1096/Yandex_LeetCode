bb12d7fdb2a3a972a3fc581dbe26b88cb0fcd73f
Sura <suri.murodova@gmail.com>
2024-02-21 09:52:13 +0300

    Add solution

--- X-Merge_Sorted_Array.py
+++ X-Merge_Sorted_Array.py
@@ -47,7 +47,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        pass
+        i = m + n - 1
+
+        while m > 0 and n > 0:
+            if nums1[m - 1] <= nums2[n - 1]:
+                nums1[i] = nums2[n - 1]
+                n -= 1
+            else:
+                nums1[i] = nums1[m - 1]
+                m -= 1
+            i -= 1
+        while n > 0:
+            nums1[i] = nums2[n - 1]
+            n -= 1
+            i -= 1
 
 
 sol = Solution()
