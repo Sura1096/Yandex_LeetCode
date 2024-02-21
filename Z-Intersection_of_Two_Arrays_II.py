627f73a022b6ca07eb82b536872eb82be1555e41
Sura <suri.murodova@gmail.com>
2024-02-21 11:17:24 +0300

    Add testcases

--- Z-Intersection_of_Two_Arrays_II.py
+++ Z-Intersection_of_Two_Arrays_II.py
@@ -32,3 +32,13 @@ the memory at once?
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         pass
+
+
+sol = Solution()
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+print(sol.intersect(nums1, nums2))
+
+nums1 = [4, 9, 5]
+nums2 = [9, 4, 9, 8, 4]
+print(sol.intersect(nums1, nums2))
