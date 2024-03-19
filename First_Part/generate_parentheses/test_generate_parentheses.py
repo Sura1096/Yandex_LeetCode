e79f8cd2e52de4a8110eec4c91784ba03c916eaf
Sura <suri.murodova@gmail.com>
2024-03-19 14:51:40 +0300

    Add two unittest methods and testcases

---
+++ First_Part/generate_parentheses/test_generate_parentheses.py
@@ -0,0 +1,21 @@
+import unittest
+from generate_parentheses import Solution
+
+
+class TestGenerateParentheses(unittest.TestCase):
+    def setUp(self):
+        self.obj = Solution()
+
+    def test_generate_parentheses(self):
+        self.assertEqual(
+            self.obj.generateParentheses(3),
+            ["((()))", "(()())", "(())()", "()(())", "()()()"]
+        )
+        self.assertEqual(
+            self.obj.generateParentheses(1),
+            ["()"]
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
