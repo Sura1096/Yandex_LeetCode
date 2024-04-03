97a276c3ae531e3c2e4d97264bcd2c25cacb93c5
Sura <suri.murodova@gmail.com>
2024-04-03 10:01:39 +0300

    Add solution

--- First_Part/trapping_rain_water/trapping_rain_water.py
+++ First_Part/trapping_rain_water/trapping_rain_water.py
@@ -20,3 +20,38 @@ n == height.length
 1 <= n <= 2 * 10^4
 0 <= height[i] <= 10^5
 '''
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        max_height = 0
+        for i in range(len(height)):
+            if height[i] > height[max_height]:
+                max_height = i
+
+        result = 0
+        left = 0
+        for right in range(max_height):
+            if left < height[right]:
+                left = height[right]
+            result += left - height[right]
+
+        left = 0
+        for right in range(len(height) - 1, max_height, -1):
+            if left < height[right]:
+                left = height[right]
+            result += left - height[right]
+
+        return result
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    print(sol.trap(height))
+
+    height = [4, 2, 0, 3, 2, 5]
+    print(sol.trap(height))
+
+    height = [1]
+    print(sol.trap(height))
