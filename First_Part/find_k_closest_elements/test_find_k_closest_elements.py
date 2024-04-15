ba322f646bfbe2890cdbf23ab34df09e5a431917
Sura <suri.murodova@gmail.com>
2024-04-15 18:36:21 +0300

    Write simple tests for find_k_closest_elements.py module

---
+++ First_Part/find_k_closest_elements/test_find_k_closest_elements.py
@@ -0,0 +1,25 @@
+from find_k_closest_elements import Solution
+import pytest
+
+sol = Solution()
+
+
+@pytest.mark.parametrize('array, k, x, expected_result', [([1, 2, 3, 4, 5], 4, 3, [1, 2, 3, 4]),
+                                                          ([1, 2, 3, 4, 5], 4, -1, [1, 2, 3, 4]),
+                                                          ([1], 1, 10, [1])])
+def test_findClosestElements_func(array, k, x, expected_result):
+    assert sol.findClosestElements(array, k, x) == expected_result
+
+
+@pytest.mark.parametrize('array, k, x', [([1, 2, 3, 4, 5], 4, "a"),
+                                         ([1, 2, 3, 4, 5], 4, "-1")])
+def test_type_error(array, k, x):
+    with pytest.raises(TypeError):
+        sol.findClosestElements(array, k, x)
+
+
+@pytest.mark.parametrize('array, k, x', [([1, 2, 3, 4, 5], "4", 3),
+                                         ([1, 2, 3, 4, 5], "4", -1)])
+def test_index_error(array, k, x):
+    with pytest.raises(IndexError):
+        sol.findClosestElements(array, k, x)
