392530571ce1c7cf03ee228b6fe4cce8919e5311
Sura <suri.murodova@gmail.com>
2024-04-15 19:38:11 +0300

    Divide tests using classes

--- First_Part/find_k_closest_elements/test_find_k_closest_elements.py
+++ First_Part/find_k_closest_elements/test_find_k_closest_elements.py
@@ -4,22 +4,23 @@ import pytest
 sol = Solution()
 
 
-@pytest.mark.parametrize('array, k, x, expected_result', [([1, 2, 3, 4, 5], 4, 3, [1, 2, 3, 4]),
-                                                          ([1, 2, 3, 4, 5], 4, -1, [1, 2, 3, 4]),
-                                                          ([1], 1, 10, [1])])
-def test_findClosestElements_func(array, k, x, expected_result):
-    assert sol.findClosestElements(array, k, x) == expected_result
+class TestFindClosestElements:
+    @pytest.mark.parametrize('array, k, x, expected_result', [([1, 2, 3, 4, 5], 4, 3, [1, 2, 3, 4]),
+                                                              ([1, 2, 3, 4, 5], 4, -1, [1, 2, 3, 4]),
+                                                              ([1], 1, 10, [1])])
+    def test_findClosestElements_func(self, array, k, x, expected_result):
+        assert sol.findClosestElements(array, k, x) == expected_result
 
 
-@pytest.mark.parametrize('array, k, x', [([1, 2, 3, 4, 5], 4, "a"),
-                                         ([1, 2, 3, 4, 5], 4, "-1")])
-def test_type_error(array, k, x):
-    with pytest.raises(TypeError):
-        sol.findClosestElements(array, k, x)
+class TestErrors:
+    @pytest.mark.parametrize('array, k, x', [([1, 2, 3, 4, 5], 4, "a"),
+                                             ([1, 2, 3, 4, 5], 4, "-1")])
+    def test_type_error(self, array, k, x):
+        with pytest.raises(TypeError):
+            sol.findClosestElements(array, k, x)
 
-
-@pytest.mark.parametrize('array, k, x', [([1, 2, 3, 4, 5], "4", 3),
-                                         ([1, 2, 3, 4, 5], "4", -1)])
-def test_index_error(array, k, x):
-    with pytest.raises(IndexError):
-        sol.findClosestElements(array, k, x)
+    @pytest.mark.parametrize('array, k, x', [([1, 2, 3, 4, 5], "4", 3),
+                                             ([1, 2, 3, 4, 5], "4", -1)])
+    def test_index_error(self, array, k, x):
+        with pytest.raises(IndexError):
+            sol.findClosestElements(array, k, x)
