e8133ee794735a4917d1faa8c60faa3fa5dcfba9
Sura <suri.murodova@gmail.com>
2024-04-17 17:35:07 +0300

    Fix typo for constraints

--- First_Part/valid_palindrome/valid_palindrome.py
+++ First_Part/valid_palindrome/valid_palindrome.py
@@ -23,7 +23,7 @@ Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
 
 Constraints:
-1 <= s.length <= 2 * 105
+1 <= s.length <= 2 * 10^5
 s consists only of printable ASCII characters.
 '''
 
