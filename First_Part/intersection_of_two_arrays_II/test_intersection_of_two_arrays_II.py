629df59a1420805bcf798152516073f82388e5c5
Sura <suri.murodova@gmail.com>
2024-04-19 16:27:09 +0300

    Add tests for boundary cases

--- First_Part/intersection_of_two_arrays_II/test_intersection_of_two_arrays_II.py
+++ First_Part/intersection_of_two_arrays_II/test_intersection_of_two_arrays_II.py
@@ -12,3 +12,12 @@ sol = Solution()
                           ([1, 1, 1, 1, 3, 4, 5, 5], [1, 1, 9, 4, 9, 8, 4], [1, 1, 4])])
 def test_positive_cases(nums1, nums2, expected_result):
     assert sol.intersect(nums1, nums2) == expected_result
+
+
+@pytest.mark.parametrize('nums1, nums2, expected_result',
+                         [([1], [3, 4, 5, 6, 7], []),
+                          ([1, 2, 3, 4, 5, 6], [7, 8, 9, 10], []),
+                          ([1], [2], []),
+                          ([1, 2, 3, 4, 5], [111], [])])
+def test_boundary_cases(nums1, nums2, expected_result):
+    assert sol.intersect(nums1, nums2) == expected_result
