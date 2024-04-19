a85126c397bdc3c8753e3ac821e02c429130f9b5
Sura <suri.murodova@gmail.com>
2024-04-19 10:54:27 +0300

    Add solution

--- First_Part/find_fist_and_last_position_of_element_in_sorted_array/find_first_and_last_position_of_element_in_sorted_array.py
+++ First_Part/find_fist_and_last_position_of_element_in_sorted_array/find_first_and_last_position_of_element_in_sorted_array.py
@@ -27,3 +27,43 @@ Constraints:
 nums is a non-decreasing array.
 -10^9 <= target <= 10^9
 '''
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.bin_search(nums, target, True)
+        right = self.bin_search(nums, target, False)
+        return [left, right]
+
+    def bin_search(self, nums: List[int], target: int, leftBias: bool):
+        start = 0
+        end = len(nums) - 1
+        i = -1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] > target:
+                end = mid - 1
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                i = mid
+                if leftBias:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+        return i
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = [5, 7, 7, 8, 8, 10]
+    target = 8
+    print(sol.searchRange(nums, target))
+
+    nums = [5, 7, 7, 8, 8, 10]
+    target = 9
+    print(sol.searchRange(nums, target))
+
+    nums = [4, 5, 8, 8, 8, 9, 10]
+    target = 8
+    print(sol.searchRange(nums, target))
