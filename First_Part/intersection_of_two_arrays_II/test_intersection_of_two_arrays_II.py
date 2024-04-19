d8762de1335f4775249ca6f05d4ef8dca1abe607
Sura <suri.murodova@gmail.com>
2024-04-19 16:34:20 +0300

    Add tests for error cases

--- First_Part/intersection_of_two_arrays_II/test_intersection_of_two_arrays_II.py
+++ First_Part/intersection_of_two_arrays_II/test_intersection_of_two_arrays_II.py
@@ -21,3 +21,18 @@ def test_positive_cases(nums1, nums2, expected_result):
                           ([1, 2, 3, 4, 5], [111], [])])
 def test_boundary_cases(nums1, nums2, expected_result):
     assert sol.intersect(nums1, nums2) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, nums1, nums2',
+                         [(ValueError, [0] * 1001, [0, 0]),
+                          (ValueError, [0, 0], [1] * 1001),
+                          (ValueError, [0] * 1001, [0] * 1001),
+                          (ValueError, [], [1, 2]),
+                          (ValueError, [1, 2, 3, 4], []),
+                          (ValueError, [], []),
+                          (ValueError, [1000000, 2, 3, 4, 5], [1, 2, 3]),
+                          (ValueError, [1, 2, 3, 4], [1, 4, 5, 600000]),
+                          (ValueError, [1, 1, 1, 500000], [5000000, 3, 4, 6])])
+def test_errors(expected_exception, nums1, nums2):
+    with pytest.raises(expected_exception):
+        sol.intersect(nums1, nums2)
