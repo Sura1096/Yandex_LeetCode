c928d9009203c4b5862d9110ce2610b071761382
Sura <suri.murodova@gmail.com>
2024-04-22 16:47:38 +0300

    Add test for error cases

--- First_Part/longest_subarray_of_1_after_deleting_one_element/test_longest_subarray_of_1.py
+++ First_Part/longest_subarray_of_1_after_deleting_one_element/test_longest_subarray_of_1.py
@@ -18,3 +18,14 @@ def test_positive_cases(nums, expected_result):
                           ([0, 0, 0, 0], 0)])
 def test_boundary_cases(nums, expected_result):
     assert sol.longestSubarray(nums) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, nums',
+                         [(ValueError, []),
+                          (ValueError, [1] * (10**5+1)),
+                          (ValueError, [0] * (10**5+1)),
+                          (ValueError, [1, 1, 1, 2, 3, 0, 0]),
+                          (ValueError, [2, 1, 1, 1, 0, 0, 1, 1])])
+def test_errors(expected_exception, nums):
+    with pytest.raises(expected_exception):
+        sol.longestSubarray(nums)
