97ff4b8277956566c001f01de629a52ada54a915
Sura <suri.murodova@gmail.com>
2024-04-22 11:47:43 +0300

    Add solution

--- First_Part/perfect_squares/perfect_squares.py
+++ First_Part/perfect_squares/perfect_squares.py
@@ -19,3 +19,25 @@ Explanation: 13 = 4 + 9.
 Constraints:
 1 <= n <= 10^4
 '''
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [n] * (n + 1)
+        dp[0] = 0
+        for target in range(1, n + 1):
+            for s in range(1, target + 1):
+                square = s * s
+                if target - square < 0:
+                    break
+                dp[target] = min(dp[target], 1 + dp[target - square])
+        return dp[n]
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    n = 12
+    print(sol.numSquares(n))
+
+    n = 13
+    print(sol.numSquares(n))
