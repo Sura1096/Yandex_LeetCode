3f42fc85bd2aadaeebfb50eee1570818d93135d4
Sura <suri.murodova@gmail.com>
2024-04-22 16:08:30 +0300

    Delete 1 testcase from test_positive test function

--- First_Part/longest_palindromic_substring/test_longest_palindromic_substring.py
+++ First_Part/longest_palindromic_substring/test_longest_palindromic_substring.py
@@ -8,7 +8,6 @@ sol = Solution()
 @pytest.mark.parametrize('s, expected_result',
                          [("babad", "bab"),
                           ("cbbd", "bb"),
-                          ("acvb", "a"),
                           ("xabax", "xabax"),
                           ("xabay", "aba")])
 def test_positive(s, expected_result):
