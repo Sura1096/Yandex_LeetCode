d6f915fea922625e7db39a8ad2b5d5b77c574f3d
Sura <suri.murodova@gmail.com>
2024-04-22 16:35:23 +0300

    Add test for positive cases

--- First_Part/longest_subarray_of_1_after_deleting_one_element/test_longest_subarray_of_1.py
+++ First_Part/longest_subarray_of_1_after_deleting_one_element/test_longest_subarray_of_1.py
@@ -3,3 +3,11 @@ from longest_subarray_of_1_after_deleting_one_element import Solution
 
 
 sol = Solution()
+
+
+@pytest.mark.parametrize('nums, expected_result',
+                         [([1, 1, 0, 1], 3),
+                          ([0, 1, 1, 1, 0, 1, 1, 0, 1], 5),
+                          ([1, 1, 1], 2)])
+def test_positive_cases(nums, expected_result):
+    assert sol.longestSubarray(nums) == expected_result
