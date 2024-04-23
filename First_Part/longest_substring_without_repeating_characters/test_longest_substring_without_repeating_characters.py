11fb5dff694b8b41d1bf5b12e972ebb5db643c41
Sura <suri.murodova@gmail.com>
2024-04-23 16:14:14 +0300

    Add test for positive cases

--- First_Part/longest_substring_without_repeating_characters/test_longest_substring_without_repeating_characters.py
+++ First_Part/longest_substring_without_repeating_characters/test_longest_substring_without_repeating_characters.py
@@ -3,3 +3,11 @@ from longest_substring_without_repeating_characters import Solution
 
 
 sol = Solution()
+
+
+@pytest.mark.parametrize('s, expected_result',
+                         [("abcabcbb", 3),
+                          ("pwwkew", 3),
+                          ("asdfghj", 7)])
+def test_positive_cases(s, expected_result):
+    assert sol.lengthOfLongestSubstring(s) == expected_result
