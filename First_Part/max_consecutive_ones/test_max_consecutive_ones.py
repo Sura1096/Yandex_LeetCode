4f3e1f115c5d0a3536cf32c3e5e49beeaed69937
Sura <suri.murodova@gmail.com>
2024-04-23 16:46:48 +0300

    Add test for positive cases

--- First_Part/max_consecutive_ones/test_max_consecutive_ones.py
+++ First_Part/max_consecutive_ones/test_max_consecutive_ones.py
@@ -3,3 +3,11 @@ from max_consecutive_ones import Solution
 
 
 sol = Solution()
+
+
+@pytest.mark.parametrize('nums, expected_result',
+                         [([1, 1, 0, 1, 1, 1], 3),
+                          ([1, 0, 1, 1, 0, 1], 2),
+                          ([1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1], 5)])
+def test_positive_cases(nums, expected_result):
+    assert sol.findMaxConsecutiveOnes(nums) == expected_result
