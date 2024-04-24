8b42ec04cf5a8181d0a327133f838bac040db044
Sura <suri.murodova@gmail.com>
2024-04-24 16:19:38 +0300

    Add test for positive cases

--- Yandex_LeetCode/max_consecutive_ones_III/test_max_consecutive_ones_III.py
+++ Yandex_LeetCode/max_consecutive_ones_III/test_max_consecutive_ones_III.py
@@ -3,3 +3,11 @@ from max_consecutive_ones_III import Solution
 
 
 sol = Solution()
+
+
+@pytest.mark.parametrize('nums, k, expected_result',
+                         [([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2, 6),
+                          ([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3, 10),
+                          ([1, 1, 1, 0, 0, 1, 1, 0], 4, 8)])
+def test_positive_cases(nums, k, expected_result):
+    assert sol.longestOnes(nums, k) == expected_result
