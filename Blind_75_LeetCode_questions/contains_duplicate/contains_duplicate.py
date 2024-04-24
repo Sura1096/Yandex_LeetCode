4e57733c08209d5c7106c908dc8542482996bafd
Sura <suri.murodova@gmail.com>
2024-04-24 09:25:22 +0300

    Add solution

--- Blind_75_LeetCode_questions/contains_duplicate/contains_duplicate.py
+++ Blind_75_LeetCode_questions/contains_duplicate/contains_duplicate.py
@@ -23,3 +23,28 @@ Constraints:
 1 <= nums.length <= 10^5
 -10^9 <= nums[i] <= 10^9
 '''
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        store = set()
+
+        for num in nums:
+            if num not in store:
+                store.add(num)
+            else:
+                return True
+
+        return False
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = [1, 2, 3, 1]
+    print(sol.containsDuplicate(nums))
+
+    nums = [1, 2, 3, 4]
+    print(sol.containsDuplicate(nums))
+
+    nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+    print(sol.containsDuplicate(nums))
