f14c3feab4544085eaf120ed1108468f63d66d94
Sura <suri.murodova@gmail.com>
2024-04-24 16:44:11 +0300

    Add test for error cases

--- Yandex_LeetCode/max_consecutive_ones_III/test_max_consecutive_ones_III.py
+++ Yandex_LeetCode/max_consecutive_ones_III/test_max_consecutive_ones_III.py
@@ -27,3 +27,17 @@ def test_positive_cases(nums, k, expected_result):
                           ([1], 1, 1)])
 def test_boundary_cases(nums, k, expected_result):
     assert sol.longestOnes(nums, k) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, nums, k',
+                         [(ValueError, [], 1),
+                          (ValueError, [1, 1, 1, 1], 10),
+                          (ValueError, [1] * (10**5 + 1), 2),
+                          (ValueError, [0, 1, 0, 1], -1),
+                          (ValueError, [1, 1, 1, 0, 0, 1, 3], 2),
+                          (ValueError, [1, 2, 3, 4, 5], 3),
+                          (ValueError, [1, 1, 1, 'a', 0, 0], 4),
+                          (ValueError, ['a', 'b', 'c'], 0)])
+def test_errors(expected_exception, nums, k):
+    with pytest.raises(expected_exception):
+        sol.longestOnes(nums, k)
