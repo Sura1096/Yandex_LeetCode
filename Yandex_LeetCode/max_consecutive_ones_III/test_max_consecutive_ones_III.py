b19f37477b40ec8523415827dcbd706744b19d82
Sura <suri.murodova@gmail.com>
2024-04-24 16:26:23 +0300

    Add test for boundary cases

--- Yandex_LeetCode/max_consecutive_ones_III/test_max_consecutive_ones_III.py
+++ Yandex_LeetCode/max_consecutive_ones_III/test_max_consecutive_ones_III.py
@@ -8,6 +8,19 @@ sol = Solution()
 @pytest.mark.parametrize('nums, k, expected_result',
                          [([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2, 6),
                           ([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3, 10),
+                          ([1, 1, 1, 0, 0, 1, 1, 0], 3, 8),
                           ([1, 1, 1, 0, 0, 1, 1, 0], 4, 8)])
 def test_positive_cases(nums, k, expected_result):
+    assert sol.longestOnes(nums, k) == expected_result
+
+
+@pytest.mark.parametrize('nums, k, expected_result',
+                         [([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 0, 4),
+                          ([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 0, 4),
+                          ([1, 1, 1, 0, 0, 1, 1, 0], 0, 3),
+                          ([1, 1, 1, 1, 1, 1, 1, 1], 3, 8),
+                          ([1, 1, 1, 1, 1, 1, 1, 1], 3, 8),
+                          ([0, 0, 0, 0, 0, 0, 0, 0], 0, 0),
+                          ([0, 0, 0, 0, 0, 0, 0, 0], 8, 8)])
+def test_boundary_cases(nums, k, expected_result):
     assert sol.longestOnes(nums, k) == expected_result
