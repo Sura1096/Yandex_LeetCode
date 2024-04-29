be5b9373c40cc9f038afe68a8eabb2b661e62922
Sura <suri.murodova@gmail.com>
2024-04-29 16:40:22 +0300

    Add test for positive cases

--- Yandex_LeetCode/maximize_distance_to_closest_person/test_maximize_distance_to_closest_person.py
+++ Yandex_LeetCode/maximize_distance_to_closest_person/test_maximize_distance_to_closest_person.py
@@ -1,2 +1,13 @@
 import pytest
 from maximize_distance_to_closest_person import Solution
+
+
+sol = Solution()
+
+
+@pytest.mark.parametrize('seats, expected_result',
+                         [([1, 0, 0, 0, 1, 0, 1], 2),
+                          ([1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0], 6),
+                          ([0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0], 3)])
+def test_positive_cases(seats, expected_result):
+    assert sol.maxDistToClosest(seats) == expected_result
