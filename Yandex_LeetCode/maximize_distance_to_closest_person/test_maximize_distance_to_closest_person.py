066c46c11c74f3751dddc3d3cfe74a67cf4b387f
Sura <suri.murodova@gmail.com>
2024-04-29 16:47:28 +0300

    Add test for error cases

--- Yandex_LeetCode/maximize_distance_to_closest_person/test_maximize_distance_to_closest_person.py
+++ Yandex_LeetCode/maximize_distance_to_closest_person/test_maximize_distance_to_closest_person.py
@@ -20,3 +20,14 @@ def test_positive_cases(seats, expected_result):
                           ([0, 0, 0, 0, 0, 0, 0, 0, 1], 8)])
 def test_boundary_cases(seats, expected_result):
     assert sol.maxDistToClosest(seats) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, seats',
+                         [(ValueError, [1]),
+                          (ValueError, [1, 1, 1, 1, 1, 1]),
+                          (ValueError, [0, 0, 0, 0, 0, 0, 0]),
+                          (ValueError, [1, 0, 'a', 0]),
+                          (ValueError, [1, 0] * (2 * (10**4) + 1))])
+def test_errors(expected_exception, seats):
+    with pytest.raises(expected_exception):
+        sol.maxDistToClosest(seats)
