43e109aa40665b99892a6194bacf47f7119dea06
Sura <suri.murodova@gmail.com>
2024-04-29 16:42:54 +0300

    Add test for boundary cases

--- Yandex_LeetCode/maximize_distance_to_closest_person/test_maximize_distance_to_closest_person.py
+++ Yandex_LeetCode/maximize_distance_to_closest_person/test_maximize_distance_to_closest_person.py
@@ -11,3 +11,12 @@ sol = Solution()
                           ([0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0], 3)])
 def test_positive_cases(seats, expected_result):
     assert sol.maxDistToClosest(seats) == expected_result
+
+
+@pytest.mark.parametrize('seats, expected_result',
+                         [([1, 0], 1),
+                          ([1, 0, 0, 0], 3),
+                          ([1, 1, 1, 1, 1, 0], 1),
+                          ([0, 0, 0, 0, 0, 0, 0, 0, 1], 8)])
+def test_boundary_cases(seats, expected_result):
+    assert sol.maxDistToClosest(seats) == expected_result
