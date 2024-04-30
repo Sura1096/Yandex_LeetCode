816ef9ff8e09f803f9092cc5338389724cf63538
Sura <suri.murodova@gmail.com>
2024-04-30 09:49:27 +0300

    Add solution

--- Blind_75_LeetCode_questions/container_with_most_water/container_with_most_water.py
+++ Blind_75_LeetCode_questions/container_with_most_water/container_with_most_water.py
@@ -24,3 +24,28 @@ n == height.length
 2 <= n <= 10^5
 0 <= height[i] <= 10^4
 '''
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                max_area = max(max_area, (right - left) * height[left])
+                left += 1
+            else:
+                max_area = max(max_area, (right - left) * height[right])
+                right -= 1
+        return max_area
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    print(sol.maxArea(height))
+
+    height = [1, 1]
+    print(sol.maxArea(height))
