ffe481fe34306abebc1f5e845a4e0903220397bf
Sura <suri.murodova@gmail.com>
2024-04-30 16:28:26 +0300

    Add test for positive testcases

--- Yandex_LeetCode/merge_sorted_array/test_merge_sorted_array.py
+++ Yandex_LeetCode/merge_sorted_array/test_merge_sorted_array.py
@@ -3,3 +3,20 @@ from merge_sorted_array import Solution
 
 
 sol = Solution()
+
+
+@pytest.mark.parametrize('nums1, m, nums2, n, expected_result',
+                         [([1, 2, 3, 0, 0, 0], 3,
+                           [2, 5, 6], 3,
+                           [1, 2, 2, 3, 5, 6]),
+                          ([1, 3, 5, 0, 0, 0], 3,
+                           [2, 4, 6], 3,
+                           [1, 2, 3, 4, 5, 6]),
+                          ([0, 0, 0], 0,
+                           [1, 2, 3], 3,
+                           [1, 2, 3]),
+                          ([4, 5, 6, 7, 0, 0, 0], 4,
+                           [1, 2, 3], 3,
+                           [1, 2, 3, 4, 5, 6, 7])])
+def test_positive_cases(nums1, m, nums2, n, expected_result):
+    assert sol.merge(nums1, m, nums2, n) == expected_result
