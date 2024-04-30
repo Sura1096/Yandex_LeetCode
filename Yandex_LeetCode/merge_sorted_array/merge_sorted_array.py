179187f50eaaea0fbefcc5adaf3772e422ba9462
Sura <suri.murodova@gmail.com>
2024-04-30 16:12:01 +0300

    Change execution part of a code

--- Yandex_LeetCode/merge_sorted_array/merge_sorted_array.py
+++ Yandex_LeetCode/merge_sorted_array/merge_sorted_array.py
@@ -63,21 +63,22 @@ class Solution:
             i -= 1
 
 
-sol = Solution()
-nums1 = [1, 2, 3, 0, 0, 0]
-m = 3
-nums2 = [2, 5, 6]
-n = 3
-print(sol.merge(nums1, m, nums2, n))
+if __name__ == '__main__':
+    sol = Solution()
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    print(sol.merge(nums1, m, nums2, n))
 
-nums1 = [1]
-m = 1
-nums2 = []
-n = 0
-print(sol.merge(nums1, m, nums2, n))
+    nums1 = [1]
+    m = 1
+    nums2 = []
+    n = 0
+    print(sol.merge(nums1, m, nums2, n))
 
-nums1 = [0]
-m = 0
-nums2 = [1]
-n = 1
-print(sol.merge(nums1, m, nums2, n))
+    nums1 = [0]
+    m = 0
+    nums2 = [1]
+    n = 1
+    print(sol.merge(nums1, m, nums2, n))
