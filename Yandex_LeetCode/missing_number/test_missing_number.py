6d467b54074b99e47674ecca0a96db364ad1879f
Sura <suri.murodova@gmail.com>
2024-05-01 16:32:37 +0300

    Add test for error cases

--- Yandex_LeetCode/missing_number/test_missing_number.py
+++ Yandex_LeetCode/missing_number/test_missing_number.py
@@ -19,3 +19,14 @@ def test_positive_cases(nums, expected_result):
                           ([0], 1)])
 def test_boundary_cases(nums, expected_result):
     assert sol.missingNumber(nums) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, nums',
+                         [(ValueError, []),
+                          (ValueError, [i for i in range(10**4 + 3)]),
+                          (ValueError, [0, 1, 2, 3, 3, 4]),
+                          (ValueError, [1, 2, 3, -1, 0]),
+                          (ValueError, [3, 4, 5])])
+def test_errors(expected_exception, nums):
+    with pytest.raises(expected_exception):
+        sol.missingNumber(nums)
