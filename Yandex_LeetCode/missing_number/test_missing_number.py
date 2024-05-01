27fb890d6fea358c74e9a3d0b2e97b052c10761a
Sura <suri.murodova@gmail.com>
2024-05-01 16:25:42 +0300

    Add test for positive cases

--- Yandex_LeetCode/missing_number/test_missing_number.py
+++ Yandex_LeetCode/missing_number/test_missing_number.py
@@ -3,3 +3,12 @@ from missing_number import Solution
 
 
 sol = Solution()
+
+
+@pytest.mark.parametrize('nums, expected_result',
+                         [([3, 0, 1], 2),
+                          ([0, 1], 2),
+                          ([9, 6, 4, 2, 3, 5, 7, 0, 1], 8),
+                          ([5, 2, 1, 3, 0], 4)])
+def test_positive_cases(nums, expected_result):
+    assert sol.missingNumber(nums) == expected_result
