ed70bf8113157943210d627578d0d5596b92aa8b
Sura <suri.murodova@gmail.com>
2024-05-02 16:14:56 +0300

    Add test for the positive cases

--- Yandex_LeetCode/move_zeroes/test_move_zeroes.py
+++ Yandex_LeetCode/move_zeroes/test_move_zeroes.py
@@ -3,3 +3,11 @@ from move_zeroes import Solution
 
 
 sol = Solution()
+
+
+@pytest.mark.parametrize('nums, expected_result',
+                         [([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]),
+                          ([0, 0, 0, 1, 0, 3, 12, 0], [1, 3, 12, 0, 0, 0, 0, 0]),
+                          ([1, 2, 4, 0, 3, 0, 12, 0], [1, 2, 4, 3, 12, 0, 0, 0])])
+def test_positive_cases(nums, expected_result):
+    assert sol.moveZeroes(nums) == expected_result
