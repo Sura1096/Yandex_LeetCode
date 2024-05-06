b8889e1b1c09a8dedb3376f58b12dc9c17276008
Sura <suri.murodova@gmail.com>
2024-05-06 15:36:38 +0300

    Add test for boundary cases

--- Yandex_LeetCode/perfect_squares/test_perfect_squares.py
+++ Yandex_LeetCode/perfect_squares/test_perfect_squares.py
@@ -12,3 +12,12 @@ sol = Solution()
                           (132, 3)])
 def test_positive_cases(n, expected_result):
     assert sol.numSquares(n) == expected_result
+
+
+@pytest.mark.parametrize('n, expected_result',
+                         [(1, 1),
+                          (4, 1),
+                          (9, 1),
+                          (100, 1)])
+def test_boundary_cases(n, expected_result):
+    assert sol.numSquares(n) == expected_result
