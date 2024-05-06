4d580c5c5fbf43b7b7c436c7bdf1df4203161e35
Sura <suri.murodova@gmail.com>
2024-05-06 16:04:02 +0300

    Add test for positive cases

--- Yandex_LeetCode/permutation_in_string/test_permutation_in_string.py
+++ Yandex_LeetCode/permutation_in_string/test_permutation_in_string.py
@@ -5,3 +5,10 @@ from permutation_in_string import Solution
 sol = Solution()
 
 
+@pytest.mark.parametrize('s1, s2, expected_result',
+                         [("ab", "eidbaooo", True),
+                          ("ab", "eidaboaoo", True),
+                          ("abc", "eidboaooabc", True),
+                          ("abc", "eidboaoobac", True)])
+def test_positive_cases(s1, s2, expected_result):
+    assert sol.checkInclusion(s1, s2) == expected_result
