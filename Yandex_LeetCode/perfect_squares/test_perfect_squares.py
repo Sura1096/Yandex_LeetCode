740ead681d8beb2f7effaed375af7f443b520cf5
Sura <suri.murodova@gmail.com>
2024-05-06 15:33:46 +0300

    Add test for positive cases

--- Yandex_LeetCode/perfect_squares/test_perfect_squares.py
+++ Yandex_LeetCode/perfect_squares/test_perfect_squares.py
@@ -3,3 +3,12 @@ from perfect_squares import Solution
 
 
 sol = Solution()
+
+
+@pytest.mark.parametrize('n, expected_result',
+                         [(12, 3),
+                          (13, 2),
+                          (15, 4),
+                          (132, 3)])
+def test_positive_cases(n, expected_result):
+    assert sol.numSquares(n) == expected_result
