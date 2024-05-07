48f8b113dbdb78cb3803b13745f8e4194df818d8
Sura <suri.murodova@gmail.com>
2024-05-07 15:43:15 +0300

    Add test with negative numbers

--- Yandex_LeetCode/rotate_image/test_rotate_image.py
+++ Yandex_LeetCode/rotate_image/test_rotate_image.py
@@ -14,3 +14,12 @@ sol = Solution()
                            [[6, 12, 1, 3], [8, 13, 2, 5], [17, 14, 3, 7], [11, 15, 4, 9]])])
 def test_positive_numbers(matrix, expected_result):
     assert sol.rotate(matrix) == expected_result
+
+
+@pytest.mark.parametrize('matrix, expected_result',
+                         [([[-1, -2, -3], [-4, -5, -6], [-7, -8, -9]],
+                           [[-7, -4, -1], [-8, -5, -2], [-9, -6, -3]]),
+                          ([[-3, -5, -7, -9], [-1, -2, -3, -4], [-12, -13, -14, -15], [-6, -8, -17, -11]],
+                           [[-6, -12, -1, -3], [-8, -13, -2, -5], [-17, -14, -3, -7], [-11, -15, -4, -9]])])
+def test_negative_numbers(matrix, expected_result):
+    assert sol.rotate(matrix) == expected_result
