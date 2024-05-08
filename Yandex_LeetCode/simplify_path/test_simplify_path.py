afb8e9ae5a22c849d8be6a3ed9217cad840fab2f
Sura <suri.murodova@gmail.com>
2024-05-08 17:20:33 +0300

    Add test for positive cases

--- Yandex_LeetCode/simplify_path/test_simplify_path.py
+++ Yandex_LeetCode/simplify_path/test_simplify_path.py
@@ -5,3 +5,11 @@ from simplify_path import Solution
 sol = Solution()
 
 
+@pytest.mark.parametrize('path, expected_result',
+                         [("/home/", "/home"),
+                          ("/../", "/"),
+                          ("/home//foo/", "/home/foo"),
+                          ("/home/user/Documents/../Pictures", "/home/user/Pictures"),
+                          ("/.../a/../b/c/../d/./", "/.../b/d")])
+def test_positive_cases(path, expected_result):
+    assert sol.simplifyPath(path) == expected_result
