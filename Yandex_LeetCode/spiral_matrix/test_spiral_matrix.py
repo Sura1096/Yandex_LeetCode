13960d32722c35543955fe83029226ddc245f287
Sura <suri.murodova@gmail.com>
2024-05-09 17:19:24 +0300

    Add test for boundary cases

--- Yandex_LeetCode/spiral_matrix/test_spiral_matrix.py
+++ Yandex_LeetCode/spiral_matrix/test_spiral_matrix.py
@@ -11,3 +11,14 @@ sol = Solution()
                           ([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], [1, 2, 4, 6, 8, 10, 9, 7, 5, 3])])
 def test_positive_cases(matrix, expected_result):
     assert sol.spiralOrder(matrix) == expected_result
+
+
+@pytest.mark.parametrize('matrix, expected_result',
+                         [([[1], [4]], [1, 4]),
+                          ([[1, 2]], [1, 2]),
+                          ([[1], [4], [6]], [1, 4, 6]),
+                          ([[1]], [1])])
+def test_boundary_cases(matrix, expected_result):
+    assert sol.spiralOrder(matrix) == expected_result
+
+
