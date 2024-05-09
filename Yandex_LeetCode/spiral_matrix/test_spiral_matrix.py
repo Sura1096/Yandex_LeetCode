6ad21b66832bb6f843a06647b859a1b0aab393c0
Sura <suri.murodova@gmail.com>
2024-05-09 17:16:40 +0300

    Add test for positive cases

--- Yandex_LeetCode/spiral_matrix/test_spiral_matrix.py
+++ Yandex_LeetCode/spiral_matrix/test_spiral_matrix.py
@@ -5,3 +5,9 @@ from spiral_matrix import Solution
 sol = Solution()
 
 
+@pytest.mark.parametrize('matrix, expected_result',
+                         [([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]),
+                          ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
+                          ([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], [1, 2, 4, 6, 8, 10, 9, 7, 5, 3])])
+def test_positive_cases(matrix, expected_result):
+    assert sol.spiralOrder(matrix) == expected_result
