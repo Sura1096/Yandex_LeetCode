aa53591880a1ea10ea24653ee96368cd3a285f9b
Sura <suri.murodova@gmail.com>
2024-05-09 17:20:21 +0300

    Change func name to positive numbers

--- Yandex_LeetCode/spiral_matrix/test_spiral_matrix.py
+++ Yandex_LeetCode/spiral_matrix/test_spiral_matrix.py
@@ -9,7 +9,7 @@ sol = Solution()
                          [([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]),
                           ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
                           ([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], [1, 2, 4, 6, 8, 10, 9, 7, 5, 3])])
-def test_positive_cases(matrix, expected_result):
+def test_positive_numbers(matrix, expected_result):
     assert sol.spiralOrder(matrix) == expected_result
 
 
