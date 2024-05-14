83672b9846e5a397e99f7bbec56b2d2759bbafb3
Sura <suri.murodova@gmail.com>
2024-05-14 17:18:43 +0300

    Add test for error cases

--- Yandex_LeetCode/squares_of_a_sorted_array/test_squares_of_a_sorted_array.py
+++ Yandex_LeetCode/squares_of_a_sorted_array/test_squares_of_a_sorted_array.py
@@ -20,3 +20,13 @@ def test_positive_cases(nums, expected_result):
                           ([7], [49])])
 def test_boundary_cases(nums, expected_result):
     assert sol.sortedSquares(nums) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, nums',
+                         [(ValueError, []),
+                          (ValueError, [-3]*(10**4+1)),
+                          (ValueError, [-1, 0, 1, 2 * 10**4]),
+                          (ValueError, [-10, -6, -4 * 10**4, 2, 3, 4])])
+def test_errors(expected_exception, nums):
+    with pytest.raises(expected_exception):
+        sol.sortedSquares(nums)
