912380d0dab314bcfe19bac045b1faec9bb6a5da
Sura <suri.murodova@gmail.com>
2024-05-14 17:35:30 +0300

    Add test for positive cases

--- Yandex_LeetCode/string_compression/test_string_compression.py
+++ Yandex_LeetCode/string_compression/test_string_compression.py
@@ -5,3 +5,11 @@ from string_compression import Solution
 sol = Solution()
 
 
+@pytest.mark.parametrize('chars, expected_result',
+                         [(["a", "a", "b", "b", "c"], 5),
+                          (["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"], 4),
+                          (['a', 'a', 'b', 'b', 'b', 'a', 'a', 'c', 'c'], 8),
+                          (['1', '1', '2', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '4', '5', '6', '6'], 10),
+                          (["a", "a", "a", "b", "b", "a", "a"], 6)])
+def test_positive_cases(chars, expected_result):
+    assert sol.compress(chars) == expected_result
