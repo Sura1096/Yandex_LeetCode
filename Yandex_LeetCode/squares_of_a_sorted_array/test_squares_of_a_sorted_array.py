71d56e870a96a2d887f8c1889e2c0bcd0c9d96dd
Sura <suri.murodova@gmail.com>
2024-05-14 17:10:17 +0300

    Add test for positive cases

--- Yandex_LeetCode/squares_of_a_sorted_array/test_squares_of_a_sorted_array.py
+++ Yandex_LeetCode/squares_of_a_sorted_array/test_squares_of_a_sorted_array.py
@@ -5,3 +5,9 @@ from squares_of_a_sorted_array import Solution
 sol = Solution()
 
 
+@pytest.mark.parametrize('nums, expected_result',
+                         [([-4, -1, 0, 3, 10], [0, 1, 9, 16, 100]),
+                          ([-7, -3, 2, 3, 11], [4, 9, 9, 49, 121]),
+                          ([-10, -4, -1, 11], [1, 16, 100, 121])])
+def test_positive_cases(nums, expected_result):
+    assert sol.sortedSquares(nums) == expected_result
