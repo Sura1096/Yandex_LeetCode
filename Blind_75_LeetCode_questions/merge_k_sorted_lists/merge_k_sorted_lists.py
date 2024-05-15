6eba3151ef6b58a59fd871edafc0e4fa7756fe76
Sura <suri.murodova@gmail.com>
2024-05-15 08:56:39 +0300

    Add solution O(n*logk)

--- Blind_75_LeetCode_questions/merge_k_sorted_lists/merge_k_sorted_lists.py
+++ Blind_75_LeetCode_questions/merge_k_sorted_lists/merge_k_sorted_lists.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 
 '''
@@ -37,3 +37,37 @@ The sum of lists[i].length will not exceed 10^4.
 '''
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        while len(lists) > 1:
+            lst = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                lst.append(self.mergeTwoLists(l1, l2))
+            lists = lst
+        return lists[0]
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        new = ListNode()
+        head = new
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                new.next = list1
+                list1 = list1.next
+            elif list1.val > list2.val:
+                new.next = list2
+                list2 = list2.next
+            new = new.next
+        new.next = list1 or list2
+        return head.next
