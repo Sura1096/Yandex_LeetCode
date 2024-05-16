0da93a9c5ae037f9370b764799e9c48f748359cb
Sura <suri.murodova@gmail.com>
2024-05-16 17:26:56 +0300

    Add test for negative numbers

--- Yandex_LeetCode/subarray_sum_equals_K/test_subarray_sum_equals_K.py
+++ Yandex_LeetCode/subarray_sum_equals_K/test_subarray_sum_equals_K.py
@@ -13,3 +13,11 @@ def test_positive_nums(nums, k, expected_result):
     assert sol.subarraySum(nums, k) == expected_result
 
 
+@pytest.mark.parametrize('nums, k, expected_result',
+                         [([-1, 8, -5, 9, 4, 3, -3, 4, 6, 12, 10, 1, 5, -19, -22], 0, 1),
+                          ([-1, 1, -1, 1, 0, 0, 2, -2], 0, 16),
+                          ([-1, -1, -2, -2, -2, 4, 2, 2, 3], 6, 2)])
+def test_negative_nums(nums, k, expected_result):
+    assert sol.subarraySum(nums, k) == expected_result
+
+
