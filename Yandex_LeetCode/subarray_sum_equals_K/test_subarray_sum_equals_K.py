021d5594caba5e34f75925f8c6ad13b0ce817173
Sura <suri.murodova@gmail.com>
2024-05-16 17:22:55 +0300

    Add test for positive numbers

--- Yandex_LeetCode/subarray_sum_equals_K/test_subarray_sum_equals_K.py
+++ Yandex_LeetCode/subarray_sum_equals_K/test_subarray_sum_equals_K.py
@@ -5,3 +5,11 @@ from subarray_sum_equals_K import Solution
 sol = Solution()
 
 
+@pytest.mark.parametrize('nums, k, expected_result',
+                         [([1, 1, 1], 2, 2),
+                          ([1, 2, 3], 3, 2),
+                          ([1, 2, 3, 7, 19, 2, 45, 6, 8, 34], 10, 1)])
+def test_positive_nums(nums, k, expected_result):
+    assert sol.subarraySum(nums, k) == expected_result
+
+
