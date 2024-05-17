28466301495cd0c7ce53d7746f51ba50729bb472
Sura <suri.murodova@gmail.com>
2024-05-17 09:17:18 +0300

    Add solution

--- Blind_75_LeetCode_questions/reorder_list/reorder_list.py
+++ Blind_75_LeetCode_questions/reorder_list/reorder_list.py
@@ -24,3 +24,38 @@ Constraints:
 The number of nodes in the list is in the range [1, 5 * 10^4].
 1 <= Node.val <= 1000
 '''
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # get the middle of a linked list
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        # merge two halfs
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
