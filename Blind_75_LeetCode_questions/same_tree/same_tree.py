e389544c4549df0817eb5642ba7691e3846ae8c3
Sura <suri.murodova@gmail.com>
2024-05-21 09:33:54 +0300

    Add solution

--- Blind_75_LeetCode_questions/same_tree/same_tree.py
+++ Blind_75_LeetCode_questions/same_tree/same_tree.py
@@ -25,3 +25,19 @@ The number of nodes in both trees is in the range [0, 100].
 '''
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+        return False
