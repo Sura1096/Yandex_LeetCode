4fe3f8ca97f1bdee350d2956e3af2d0f2a250dbc
Sura <suri.murodova@gmail.com>
2024-05-27 17:47:59 +0300

    Add test function to test error cases

--- Yandex_LeetCode/subarray_sums_divisible_by_k/test_subarray_sums_divisible_by_k.py
+++ Yandex_LeetCode/subarray_sums_divisible_by_k/test_subarray_sums_divisible_by_k.py
@@ -29,3 +29,17 @@ def test_negative_numbers(nums, k, expected_result):
                           ([1, 2, 3], 1000, 0)])
 def test_boundary_cases(nums, k, expected_result):
     assert sol.subarraysDivByK(nums, k) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, nums, k',
+                         [(ValueError, [], 2),
+                          (ValueError, [1, 2] * (3 * 10**4 + 1), 2),
+                          (TypeError, [1, 2, 3], 'a'),
+                          (ValueError, [1, 2, 3], 0),
+                          (ValueError, [1, 2, 3], 10 * 10**4),
+                          (TypeError, [2, 2, 2, ';', 4, 1, 3], 4),
+                          (ValueError, [7, -1, -3, 6 * -10**4, 9, 5], 110),
+                          (ValueError, [-1, -1, -2, -4, 10 * 10**4, 11, 12], 1000)])
+def test_error_cases(expected_exception, nums, k):
+    with pytest.raises(expected_exception):
+        sol.subarraysDivByK(nums, k)
