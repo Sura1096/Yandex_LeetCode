6ba6bc32c46995785793d9bf1af78ff849c009d9
Sura <suri.murodova@gmail.com>
2024-05-28 17:30:14 +0300

    Add test for error cases

--- Yandex_LeetCode/summary_ranges/test_summary_ranges.py
+++ Yandex_LeetCode/summary_ranges/test_summary_ranges.py
@@ -20,3 +20,22 @@ def test_positive_numbers(nums, expected_result):
                           ([-6, -4, -3, -1], ['-6', '-4->-3', '-1'])])
 def test_negative_numbers(nums, expected_result):
     assert sol.summaryRanges(nums) == expected_result
+
+
+@pytest.mark.parametrize('nums, expected_result',
+                         [([], []),
+                          ([0], ['0'])])
+def test_boundary_cases(nums, expected_result):
+    assert sol.summaryRanges(nums) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, nums',
+                         [(ValueError, [i for i in range(25)]),
+                          (TypeError, [1, 2, 3, 4, 'a', 6, 7]),
+                          (ValueError, [-3**31, 1, 2, 3]),
+                          (ValueError, [1, 2, 3, 3**31]),
+                          (ValueError, [1, 2, 2, 3, 3, 4]),
+                          (ValueError, [2, 5, 1, 3, 4])])
+def test_error_cases(expected_exception, nums):
+    with pytest.raises(expected_exception):
+        sol.summaryRanges(nums)
