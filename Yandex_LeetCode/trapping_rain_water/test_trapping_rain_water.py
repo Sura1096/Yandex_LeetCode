4d8fa421d1ae107ec520d5bd76bc5fbdef067dcf
Sura <suri.murodova@gmail.com>
2024-05-30 17:13:58 +0300

    Write test for positive cases with pytest

--- Yandex_LeetCode/trapping_rain_water/test_trapping_rain_water.py
+++ Yandex_LeetCode/trapping_rain_water/test_trapping_rain_water.py
@@ -1,32 +1,16 @@
-import unittest
+import pytest
 from trapping_rain_water import Solution
 
 
-class TestTrappingWater(unittest.TestCase):
-    def setUp(self):
-        self.sol = Solution()
-
-    def test_function_output(self):
-        self.assertEqual(
-            self.sol.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]),
-            6
-        )
-
-        self.assertEqual(
-            self.sol.trap([4, 2, 0, 3, 2, 5]),
-            9
-        )
-
-        self.assertEqual(
-            self.sol.trap([1]),
-            0
-        )
-
-        self.assertEqual(
-            self.sol.trap([2, 0, 2]),
-            2
-        )
+sol = Solution()
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize('height, expected_result',
+                         [([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6),
+                          ([4, 2, 0, 3, 2, 5], 9),
+                          ([3, 0, 2, 0, 4], 7),
+                          ([4, 1, 3, 0, 5], 8),
+                          ([5, 1, 0, 1, 3, 4, 2, 1, 2], 12),
+                          ([0, 1, 0, 2, 0, 3, 0, 4], 6)])
+def test_positive_cases(height, expected_result):
+    assert sol.trap(height) == expected_result
